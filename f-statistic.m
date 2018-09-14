9d4f52f508bc6d0c9ce120ecfedfc9fdd1929616
Emile Muller <emile.muller@etu.univ-amu.fr>
2018-09-14 15:17:04 +0200

    added comment

--- f-statistic.m
+++ f-statistic.m
@@ -1,6 +1,7 @@
 H = 1;
 M = 2;
 L = 3;
+% initial conditions, works with 3 categories
 IV = [9;7;6.5;8;7.5;7;9.5;8;6.5;7.5;8;6;7;6.5;7.5;8;6;6;6.5;6.5];
 DV = [H;H;H;H;H;H;H;H;H;M;M;M;M;M;M;L;L;L;L;L];
 
