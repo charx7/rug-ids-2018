7b862aed06fd10acc9d86550bae0d1d90cf32f8e
Panagiotis <pgiagkoulas@gmail.com>
2018-09-15 19:49:55 +0200

    Fixes in indexing and calculation of unbiased variance

--- f-statistic.m
+++ f-statistic.m
@@ -26,26 +26,29 @@ A = IV(1 : DV2(1));
 meanA = mean(A);
 
 % B: all the 'M' elements of IV
-B = IV(DV2(1) : DV2(1)+DV2(2));
+B = IV(DV2(1)+1 : DV2(1)+DV2(2));
 meanB = mean(B);
 
 % C: all the 'L' elements of IV
-C = IV(DV2(1)+DV2(2) : DV2(1)+DV2(2)+DV2(3));
+C = IV(DV2(1)+DV2(2)+1 : DV2(1)+DV2(2)+DV2(3));
 meanC = mean(C);
 
+% calculating weighted mean of means
 grandMean = (DV2(1) * meanA + DV2(2) * meanB + DV2(3) * meanC) / (DV2(1) + DV2(2) + DV2(3));
 
+% calculating the mean of sums square between groups
 SSB = ((DV2(1)*(meanA - grandMean).^2) + (DV2(2)*(meanB - grandMean).^2) + (DV2(3)*(meanC - grandMean).^2)) / (numel(DV2)-1);
 
 sumVarA = sum((A - meanA).^2);
-varianceA = sumVarA / DV2(1);
+varianceA = sumVarA / (DV2(1) - 1);;
 
 sumVarB = sum((B - meanB).^2);
-varianceB = sumVarB / DV2(2);
+varianceB = sumVarB / (DV2(2) - 1);;
 
 sumVarC = sum((C - meanC).^2);
-varianceC = sumVarC / DV2(3);
+varianceC = sumVarC / (DV2(3) - 1);;
 
+% calculating the mean of sums square within groups
 SSW = ((DV2(1)-1)*varianceA + (DV2(2)-1)*varianceB + (DV2(3)-1)*varianceC) / (DV2(1)-1 + DV2(2)-1 + DV2(3)-1);
 
 F = SSB / SSW
