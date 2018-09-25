3a1a37f4818fd25f24c0ff35832410f2abc90ff0
Danny Rogaar <dannyrogaar@gmail.com>
2018-09-26 01:02:59 +0200

    Now using correct word set instead of list. Cleaned some code.

--- Assignment_3/state_union_scatter.py
+++ Assignment_3/state_union_scatter.py
@@ -1,50 +1,49 @@
 # NLTK imports
 import nltk
-from nltk.corpus import webtext
 from nltk.corpus import state_union
-from nltk.probability import DictionaryProbDist
+from nltk.corpus import gutenberg
 
 import matplotlib.pyplot as plt
 import numpy as np
 from util import clean_text
-
-# Import the vectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
+from tqdm import tqdm
 from sklearn.decomposition import PCA
 
-
 nltk.download('state_union')
-nltk.download('stopwords')
-nltk.download('punkt')
+nltk.download('gutenberg')
 
-textnames = state_union.fileids()
+# Set current corpus (state_union, gutenberg)
+corpus = state_union
+
+textnames = corpus.fileids()
+corpusnames = {gutenberg : 'gutenberg', state_union : 'state_union'}
 
 print("cleaning texts..")
-clean_texts = {name : clean_text(state_union.raw(name)) for name in textnames}
-freqs = {name : nltk.FreqDist(text) for name, text in clean_texts.items()}
-print("done")
+clean_texts = {name : clean_text(corpus.raw(name)) for name in tqdm(textnames)}
+print("Calculating frequencies..")
+freqs = {name : nltk.FreqDist(text) for name, text in tqdm(clean_texts.items())}
 
 # Create complete vocabulary
-vocabulary = []
+wordlist = set()
 for freq in freqs.values():
-    vocabulary += list(freq.keys())
-vocab_range = list(range(len(vocabulary)))
-vocabulary = dict(zip(vocabulary, vocab_range))
-print(vocabulary)
+    wordlist.update(freq.keys())
+vocabulary = {word : idx for (idx, word) in enumerate(wordlist)}
 
 # Convert texts to wordcount vectors
-word_vectors = {name : np.zeros(len(vocabulary,)) for name in textnames}
+word_vectors = {name : np.zeros(len(wordlist,)) for name in textnames}
 for textname, vector in word_vectors.items():
     for word, freq in freqs[textname].items():
-        # The word count at a fixed index is increased by one
         vector[vocabulary[word]] += freq
 
+# Stack text vectors into single matrix
 data = np.stack(word_vectors.values())
-print(mat.shape)
 
+# Apply PCA to find linear word combination pc's
 _pca_ = PCA(n_components = 2)
 _pca_.fit(data)
 projection = _pca_.transform(data)
-print(projection.shape)
 
+# Plot texts on principal component dimension
 plt.scatter(projection[:, 0], projection[:, 1])
+plt.title("PCA (" + corpusnames[corpus] + ")")
+plt.show()
