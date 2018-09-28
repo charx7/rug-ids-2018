a36a61c2fd146edff951c7a40ae4aa486e374b9f
eserco <esercomak90@gmail.com>
2018-09-28 08:33:21 +0200

    completed

--- Assignment_4/stateunion_highfreq_collocations.py
+++ Assignment_4/stateunion_highfreq_collocations.py
@@ -1,6 +1,7 @@
 #Importing NLTK and download : tokenizer, tagger, stopwords, corpus
 import nltk
 import pandas as pd
+import numpy as np
 from nltk.corpus import stopwords, state_union
 from nltk.tokenize import word_tokenize
 from nltk.collocations import TrigramCollocationFinder
@@ -20,29 +21,45 @@ allTexts = " ".join(corpusList)
 
 #get english stop words
 stop_words = set(stopwords.words('english'))
-#get a sample raw txt from state_union corpus
-#stateUnionRawExp = state_union.raw(rawText)
+
+#tokenize
 tokens = word_tokenize(allTexts)
+
 #tag tokens
 tagged = pos_tag(tokens)
+
 #convert tagged tuple into dataframe for the ease of manipulation
 tagged = pd.DataFrame(list(tagged), columns=["word","type"])
+
 #turn words into lowercase except NNP and NNPS
 for i in range(len(tagged)):
     if tagged["type"][i] in ('NNP','NNPS'):
         pass
     else:
         tagged["word"][i] = tagged["word"][i].lower()
+
 #filter out stopwords and puncuations
 stopWordFiltered = [w for w in tagged["word"].values if not w in stop_words]
 puncFiltered = [w for w in stopWordFiltered if len(w)>2]
+
 #tag tokens again
 filteredTagged = pos_tag(puncFiltered)
+
+#create list for filtering tokens
+llist = ['NNP', 'NNPS', 'fiscal', 'year', 'years', '1947', '1946','guests','honored','fellow']
+
+#filter for NNP and NNPS types
+filterTypes = [(x,y) for (x,y) in filteredTagged if y not in llist]
+
+#filter for some unwanted words such as fiscal, year, etc
+filterExtraWords = [(x,y) for (x,y) in filterTypes if x not in llist]
+
 #find trigrams
-finder = TrigramCollocationFinder.from_words(filteredTagged,3)
+finder = TrigramCollocationFinder.from_words(filterExtraWords,3)
+
 #obtain frequency distribution for all trigrams
 freqDistTrigram = finder.ngram_fd.items()
 freqDistTable = pd.DataFrame(list(freqDistTrigram), columns=['collocations','freq']).sort_values(by='freq', ascending=False)
-#filter for NNP and NNPS last time
 
-#return top 10 results as dataframe
+#returns 10 trigram collocations with highest frequency within corpus
+freqDistTable.head(10)
