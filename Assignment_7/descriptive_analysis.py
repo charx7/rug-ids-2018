944f80900f7173ce161d79a6c79320fff20d5122
Carlos Huerta <carlos.huertaso@udlap.mx>
2018-10-16 10:48:37 +0200

    t-SNE dimension reduction and various plots

--- Assignment_7/descriptive_analysis.py
+++ Assignment_7/descriptive_analysis.py
@@ -3,6 +3,9 @@ print('Im Working!')
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from sklearn.manifold import TSNE
+import time
 
 # Custom Imports
 from utils.preprocessor import preprocess
@@ -22,7 +25,7 @@ print("The appended resulting dataframe is: \n", df)
 scaledDf = scale(df)
 
 # Perform PCA with 2 var on the scaledDf
-pca_df, explainedVar = doPCA(scaledDf)
+pca_df, explainedVar = doPCA(scaledDf, 20)
 print('The pca is: \n', pca_df,
     '\n the culmutative sum of the variance explained is: ',
     explainedVar.cumsum())
@@ -37,5 +40,46 @@ plt.scatter(pca_df['PCA 1'],pca_df['PCA 2'], c=colors, marker='.')
 plt.title("2 dimensions PCA representation of the dataset", fontsize=15)
 plt.xlabel('PCA_1', fontsize=13)
 plt.ylabel('PCA_2', fontsize=13)
+fig.set_size_inches(10, 10, forward=True)
+plt.show()
+
+# Set label names for the plot
+x_cords = []
+for i in range(len(explainedVar[:])):
+    x_cords.append(i + 1)
+
+# plot the PCA scree Plot
+fig, ax = plt.subplots()
+ax.plot(x_cords, explainedVar, 'o-')
+plt.title('Scree Plot')
+plt.xlabel('Principal Component')
+plt.ylabel('Eigenvalue')
+ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+plt.show()
+
+# plot the PCA culmutative variance explained Plot
+fig, ax = plt.subplots()
+ax.plot(x_cords, explainedVar.cumsum(), 'o-')
+plt.title('Culmutative Variance Plot')
+plt.xlabel('Principal Component')
+plt.ylabel("`%` of the Var explained")
+ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+plt.show()
+
+# Now we are going to do t-TSNE
+time_start = time.time()
+tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=1000)
+tsne_results = tsne.fit_transform(scaledDf.values)
+print ('t-SNE done! Time elapsed: {} seconds'.format(time.time()-time_start))
+print(tsne_results)
+# Save the resulting columns of t-SNE for a plot
+x_axis = tsne_results[:,0]
+y_axis = tsne_results[:,1]
+
+# Plot the results of the t-SNE
+plt.scatter(x_axis, y_axis, c=colors, marker='.')
+plt.title("2 dimensions t-SNE representation of the dataset", fontsize=15)
+plt.xlabel('x-tsne', fontsize=13)
+plt.ylabel('y-tsne', fontsize=13)
 fig.set_size_inches(10, 10, forward=True)
 plt.show()
--- Assignment_7/utils/dimentionallityReduction.py
+++ Assignment_7/utils/dimentionallityReduction.py
@@ -2,13 +2,18 @@ from sklearn.decomposition import PCA
 import pandas as pd
 
 # Fucntion that performs PCA TODO hardcoded to 2 increase to n
-def doPCA(scaledDf):
+def doPCA(scaledDf, dimensionsToReduce):
     # Declare an instance of a PCA object
-    pca = PCA(n_components=2)
+    pca = PCA(n_components= dimensionsToReduce)
     # Perform the fit of the pca
-    pca_components_2 = pca.fit_transform(scaledDf)
+    pca_components = pca.fit_transform(scaledDf)
+
     # dataframe out of the resulting pca results
-    pca_df = pd.DataFrame(data=pca_components_2, columns = ['PCA 1', 'PCA 2'])
+    columnLabels = []
+    for i in range(dimensionsToReduce):
+        columnLabels.append('PCA ' + str(i+1))
+
+    pca_df = pd.DataFrame(data=pca_components, columns = columnLabels)
     # Get the explained variance
     explainedVariance = pca.explained_variance_ratio_
     # Get the culmutative sum
