64b72135cae4902a18e4b71bbd8602c84364b4c6
Doppie <daan_opheikens@hotmail.com>
2018-10-21 18:19:19 +0200

    Added TODO and put imports at the right lines

--- Assignment_7/ensemble.py
+++ Assignment_7/ensemble.py
@@ -3,8 +3,6 @@ import sklearn.tree as sk
 from scipy.stats import mode
 from sklearn.neighbors import KNeighborsClassifier
 from exploratory_analysis import reducedDf
-from decisiontree import y_train
-from KNN_classification import optimal_k, X_train_scaled, y_train
 
 class Classifier:
     def __init__(self, classifier):
@@ -26,6 +24,7 @@ def majorityvote(data, classifiers):
 
 ## Decision Tree
 ##   Define and train decision tree
+from decisiontree import y_train
 classifier_dt = sk.DecisionTreeClassifier(max_depth=20, min_samples_split= 93, max_leaf_nodes=43, min_samples_leaf=2, random_state=25)
 dtModel = classifier_dt.fit(reducedDf.iloc[:, :-1], y_train)
 ##   Pass the classification function to the class
@@ -33,12 +32,16 @@ decisionTree = Classifier(classifier_dt.predict)
 
 ## KNN
 ##   Define and train KNN
+from KNN_classification import optimal_k, X_train_scaled, y_train
 classifier_knn = KNeighborsClassifier(n_neighbors=optimal_k)
 knnModel = classifier_knn.fit(X_train_scaled, y_train)
 ##   Pass the classification function to the class
 knnClassifier = Classifier(classifier_knn.predict)
 
-# Other classifiers?
+# TODO:
+#	Use different parameters for Decision Tree
+#	Use different number of neighbours for KNN
+#	Use another classifier?
 
 # Add differently initialised classifiers to the ensemble
 # Add Knn and DT classifiers to the ensemble
